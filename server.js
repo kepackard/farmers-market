b995b4be1e6eaa20afe1c0295e25bd711237b112
kepackard <kepackard@gmail.com>
2021-10-09 17:14:15 -0400

    Adds test database and first GET route

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const app = express();
+const PORT = process.env.PORT || 3000
+
+const apples = require("./models/apples.ejs")
 require("dotenv").config();
 
+//index route
+
+app.get("/apples", (req, res) =>
+    res.send(apples)
+);
 
 
-const PORT = process.env.PORT;
-app.listen (PORT, () => {console.log(`Express is listening on port:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Express is listening on port ${PORT}`)
 });
