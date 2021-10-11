b025f56f5d7921669090de340c5d03394392638b
kepackard <kepackard@gmail.com>
2021-10-11 11:01:03 -0400

    Adds workind index

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 3000
 
-const apples = require("./models/apples.ejs")
+const varieties = require("./models/varieties.js")
 require("dotenv").config();
 
 //index route
 
-app.get("/apples", (req, res) => {
-    res.render("index.ejs")
+app.get("/apples/", (req, res) => {
+    // console.log(varieties)
+    res.render("index.ejs", {
+        allVarieties: varieties,
+    })
 });
 
+//show route
+app.get("/apples/:id", (req, res) => {
+    res.render("show.ejs", {
+        variety: varieties[req.params.id],
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Express is listening on port ${PORT}`)
