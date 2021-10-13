0672931430eda932cac5d27387290697febe0b11
kepackard <kepackard@gmail.com>
2021-10-12 20:06:38 -0400

    Updates code for heroku

--- server.js
+++ server.js
@@ -1,66 +1,84 @@
+//dependencies
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 3000
-
-const varieties = require("./models/varieties.js")
+const mongoose = require("mongoose");
 require("dotenv").config();
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const Market = require("./models/markets.js")
+
+//middleware
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static("public"))
+
+//database
+const DATABASE_URL = process.env.DATABASE_URL;
+mongoose.connect(DATABASE_URL);
+const db = mongoose.connection;
+db.on('connected', () => console.log('Connected to MongoDB'));
+db.on('error', (error) => console.log('MongoDB Error ' + error.message));
+
+//ROUTES
 
 //index route
-
-app.get("/apples/", (req, res) => {
-    res.render("index.ejs", {
-        allVarieties: varieties,
-        variety: varieties,
+app.get("/markets/", (req, res) => {
+    Market.find({}, (err, foundMarkets) => {
+        res.render("index.ejs", {
+            markets: foundMarkets
+        })
     })
 });
 
-//new route
-app.get("/apples/new", (req, res) => {
-    res.render("new.ejs")
-});
+// //new route
+// app.get("/markets/new", (req, res) => {
+//     res.render("new.ejs")
+// });
 
-//delete route
-app.delete("/apples/indexOfApplesArray", (req, res) => {
-    allVarieties.splice(req.params.indexOfApplesArray,1)
-        res.redirect("/apples/")
-});
-
-
-//update route
+// //delete route
+// app.delete("/markets/:id", (req, res) => {
+//     Market.findByIdAndRemove(req.params.id, (err, data) => {
+//         res.redirect("/markets/")
+//     })
+// });
 
 
+// // //update route
+// // app.put("/markets/:indexOfMarketsArray", (req, res) => {
+// //     varieties[req.params.indexOfMarketsArray] = req.body 
+// //         res.redirect("/markets")
+// // });
 
-//edit route
-app.get("/apples/indexOfApplesArray", (req, res) => {
-    res.render("edit.ejs", 
-        {
-            allVarieties: varieties[req.params.indexOfApplesArray], 
-            index: req.params.indexOfApplesArray
-        }
-    )
-});
 
-// create route
-app.post("/apples", (req, res) => {
-    req.body = {
-        name: req.body.name, 
-        img: req.body.img,
-        date: req.body.date,
-        location: req.body.location,
-        appearance: req.body.appearance,
-        uses: req.body.appearance,
-    },
-        varieties.push(req.body);
-    res.redirect("/apples")
-});
+// //edit route
+// app.get("/markets/indexOfApplesArray", (req, res) => {
+//     res.render("edit.ejs", 
+//         {
+//             allMarkets: varieties[req.params.indexOfMarketArray], 
+//             index: req.params.indexOfMarketsArray
+//         }
+//     )
+// });
 
-//show route
-app.get("/apples/:id", (req, res) => {
-    res.render("show.ejs", {
-        variety: varieties[req.params.id],
-    })
-});
+// // create route
+// app.post("/markets", (req, res) => {
+//     req.body = {
+//         name: req.body.name, 
+//         address: req.body.address,
+//         date: req.body.date,
+//         hours: req.body.hours,
+//         wares: req.body.wares,
+//     },
+//         Market.push(req.body);
+//     res.redirect("/markets")
+// });
+
+// //show route
+// app.get("/markets/:id", (req, res) => {
+//     res.render("show.ejs", {
+//         Market: markets[req.params.id],
+//     })
+// });
 
 app.listen(PORT, () => {
     console.log(`Express is listening on port ${PORT}`)
